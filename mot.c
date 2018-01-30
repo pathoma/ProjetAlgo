59a38018ffb7b547e75708832e7e0cc67fd612fe
paul thomas <l2info032@ic2s124-12.univ-lemans.fr>
2018-01-30 16:31:40 +0100

    changment partiel en fin de tp

--- mot.c
+++ mot.c
@@ -1,22 +1,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-char tableauMot[],
+char tableauMot[40];
+int nbmot =0;
 
-void lire_fichier{
+void lire_fichier(void){
 	char nomFichier[20];
 	FILE * fichier;
 	printf("quel est le nom du fichier?");
 	scanf("%s",nomFichier);
 	fichier=fopen(nomFichier,"r");
 
-int i =0;
-char mot[20];
-while(!feof(fichier))
+	char mot[100];
+	while(!feof(fichier))
+		{
+			fscanf(fichier,"%s",&mot);
+			tableauMot[nbmot]=mot;
+			nbmot++;
+		}
+		fclose(fichier);
+}
+
+int main() {
+
+	lire_fichier();
+	for(int i=0; i<=nbmot; i++)
 	{
-		fscanf(fichier,"%s",&mot);
-		tableauMot[i]=mot;
-		i++;
+		printf("%s\n",tableauMot[i]);
+
 	}
-	fclose(fichier);
 }
