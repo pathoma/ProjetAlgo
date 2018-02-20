978c52a5c2f29e3df819bd01e43171176c88addb
Thomas Paul <l2info032@ic2s120-14.univ-lemans.fr>
2018-02-20 14:28:07 +0100

    l'écriture et la lecture dans mot.c fonctionne

--- mot.c
+++ mot.c
@@ -1,6 +1,10 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <mot.h>
+#include <string.h> 
+#include "mot.h"
+
+char *tableauMot[];
+int nbmot = 0;
 
 extern void lire_fichier(void){
 	/*char nomFichier[15]="list_ani.txt";*/
@@ -16,9 +20,29 @@ extern void lire_fichier(void){
 	while(!feof(fichier))
 		{
 			fscanf(fichier,"%s",motarecup);
-			tableauMot[nbmot]=motarecup;
+			 tableauMot[nbmot] = malloc (strlen (motarecup) + 1);
+			strcpy(tableauMot[nbmot],motarecup);
+			
 			nbmot++;
 		}
 		fclose(fichier);
+		lire_tableau_mots();
+	printf("\n");
 }
 
+ 
+extern void lire_tableau_mots(void){
+	int i;	
+	printf("\n lecture des mots \n");
+	
+	for (i=0; i<= nbmot;i++)
+	{
+		printf("%s|",tableauMot[i]);  
+	}
+	printf("\n");
+}
+
+extern  char * recup_mot(int i){
+	
+	return tableauMot[i];
+}
