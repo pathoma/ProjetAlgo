c8e6ccc2695544524a79261e957f8c9d30494d59
paul thomas <l2info032@ic2s122-12.univ-lemans.fr>
2018-04-03 14:56:54 +0200

    ajout de commentaire doxygen

--- outil.c
+++ outil.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h> 
+/**
+ * ... Documentation ...
+ * \file outil.c
+ * \brief outil.c est une liste de fonction global (qu'on pourrais utilsier tel quel dans un autre programme
+ * \author THOMAS Paul 
+ * \version 1.0
+ * \date 3 avril 2018
+*/
 
+/**
+ * \fn   lire_tableau(char * tableau[], int taille )
+ * \brief affiche un tableau de mot
+ * \param tableau tableau de mot
+ * \param taille est la taille du tableau
+ * \return un int qui est la taille du mot
+*/
 extern void lire_tableau(char * tableau[], int taille ){
 	int i;
 for (i=0; i< taille;i++)
