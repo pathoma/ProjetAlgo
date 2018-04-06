a4134a65acfc38ad67aa27b92caa25367f3651dc
fata72 <35528449+fata72@users.noreply.github.com>
2018-04-06 11:52:17 +0200

    Update premier_saisie.c

--- src/premier_saisie.c
+++ src/premier_saisie.c
@@ -144,22 +144,24 @@ int verification(char*mot,char*trouver){
 	int i=0;
 	int j=0;
 	int condi=0;
-
-	printf("motVerif : %s \n",mot);
-	printf("trouver Verif : %s \n",trouver);
+	
+	
+	//printf("motVerif : %s \n",mot);
+	//printf("trouver Verif : %s \n",trouver);
+	
 
 	for(i=0;i<taille;i++){
 		if(mot[i]!=trouver[i]){
 			condi=1;
 			i=taille;
 		}
-		printf("%i %c %c \n",i,mot[i],trouver[i]);
+		//printf("%i %c %c \n",i,mot[i],trouver[i]);
 	} 
 	
 	if(condi==1){
 		for(i=taille-1;i>=0;i--){ 
 			if(mot[j]!=trouver[i]) return 0;
-			printf("verif inverse :%i %c %c \n",i,mot[j],trouver[i]);
+			//printf("verif inverse :%i %c %c \n",i,mot[j],trouver[i]);
 			j++;
 		}
 	} else return 1;
@@ -199,34 +201,34 @@ void saisie(mat_t mat){
 	char* trouver;
 
 	do{
-	printf("Veuillez saisir le mot que vous pensez avoir trouvé (indiquer son numéro visible dans la liste au dessu de la grille : ");
+	printf("Veuillez saisir le mot que vous pensez avoir trouvé (indiquez son numéro visible dans la liste au dessus de la grille : ");
 	scanf("%i",&id);
-	}while(id<0||id>=nbliste);
+	}while(id<0||id>=nbliste);		//le numéro du mot entré par l'utilisateur doit correspondre à un numéro existant.
 	
 	
 	do{
-	printf("veuillez saisir l'adresse du DEBUT en commencent par le numéro de ligne puis le numéro de colonne : ");
+	printf("veuillez saisir l'adresse du DEBUT en commençant par le numéro de ligne puis le numéro de colonne : ");
 	scanf("%i%i",&deb1,&deb2);
-	}while(deb1>=mat.nbl||deb2>=mat.nbc||deb1<0||deb2<0);
+	}while(deb1>=mat.nbl||deb2>=mat.nbc||deb1<0||deb2<0);  //les coordonnées doivent appartenir aux dimension de la matrices
 
 	
 	do{
-	printf("veuillez saisir l'adresse de FIN en commencent par le numéro de ligne puis le numéro de colonne  : ");
+	printf("veuillez saisir l'adresse de FIN en commençant par le numéro de ligne puis le numéro de colonne  : ");
 	scanf("%i%i",&fin1,&fin2);
-	}while(fin1>=mat.nbl||fin2>=mat.nbc||fin1<0||fin2<0);
+	}while(fin1>=mat.nbl||fin2>=mat.nbc||fin1<0||fin2<0);	//les coordonnées doivent appartenir aux dimension de la matrices
 
 
-	trouver=mot_trouver(deb1,deb2,fin1,fin2,mat);
+	trouver=mot_trouver(deb1,deb2,fin1,fin2,mat);		//récupération du mot lu grâce aux coordonnées fourni par l'utilisateur
 
 	printf("\n");
 	
-	printf("%s",trouver);
+	//printf("%s",trouver);
 	printf("\n");
-	if(verification(listeMot[id],trouver)==1) {
+	if(verification(listeMot[id],trouver)==1) {		//comparaison et vérification pour savoir si un mot à été trouvé ou non
 			printf("bien joué, vous avez trouvé un mot");
-			listeMot[id]=listeMot[nbliste-1];
+			listeMot[id]=listeMot[nbliste-1];	//remplacement du mot trouvé par le dernier mot de la liste
 			nbmot=nbliste-1;
-			nbliste=nbliste-1;
+			nbliste=nbliste-1;			//réduction de la taille de la liste
 	}
 	else printf("looser ce n'est pas le bon mot \n");	
 }
@@ -261,23 +263,23 @@ void remplissage(mat_t mat){
 	char alphabet3[] = "QYXJKWZ";	   	 //lettre d'une fréquence inférieur à 1%
 	srand(time(NULL));
    
-	for(i=0;i<mat.nbl;i++){
+	for(i=0;i<mat.nbl;i++){				
 	   	for(j=0;j<mat.nbc;j++){
-	   	   	if(mat.val[i][j]=='0'){
+	   	   	if(mat.val[i][j]=='0'){		//si la case n'est pas encore rempli par une lettre
 				frec=rand()%3;
 				if(frec!=0){
-					lettre =rand()% 9;
-					printf("%i ",lettre);
+					lettre =rand()% 9;		
+					//printf("%i ",lettre);
 					mat.val[i][j]= alphabet1[lettre];
 				} else {
 					frec=rand()%3;
 					if(frec!=0){
 						lettre =rand()% 8;
-						printf("%i ",lettre);
+						//printf("%i ",lettre);
 						mat.val[i][j]= alphabet2[lettre];
 					} else {	
 						lettre =rand()% 6;
-						printf("%i ",lettre);
+						//printf("%i ",lettre);
 						mat.val[i][j]= alphabet3[lettre];
 					}
 				}		
